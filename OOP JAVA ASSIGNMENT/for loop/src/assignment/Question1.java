1d8f610f51737e203570c785d8eb2ce7b6c72fac
KhushiRajput08 <khushi.rajput.cmaug26@gmail.com>
2026-09-16 16:33:04 +0530

    Add files via upload

---
+++ OOP JAVA ASSIGNMENT/for loop/src/assignment/Question1.java
@@ -0,0 +1,16 @@
+package assignment;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+	for(int i=0;i<=1000;i++) {
+		if(i%2 != 0) {
+			System.out.println(i);
+	
+		}
+	
+	}
+
+	}
+
+}
---
+++ OOP JAVA ASSIGNMENT/for loop/src/assignment/Question2.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+	for(int i=0;i<=500;i++) {
+		if(i%2==0) {
+			System.out.println(i);
+		}
+	}
+
+	}
+
+}
